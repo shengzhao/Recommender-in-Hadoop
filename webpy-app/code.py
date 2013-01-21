9ef7995210574619eae38ed2b7f3a9bab4026598
shengzhao <zszhaoshengzs@163.com>
2013-01-21 20:34:37 +0800

    random bug

--- webpy-app/code.py
+++ webpy-app/code.py
@@ -1,6 +1,6 @@
 import web
 
-render = web.template.render('var/www/webpy-app/templates/')
+render = web.template.render('/home/hadoop/git/webpy-app/templates/')
 db = web.database(dbn='mysql', user='root', pw='zs', db='recommender')
 urls = (
 	 '/', 'index',
@@ -13,7 +13,11 @@ class index:
 
 class add:
 	def POST(self):
+		web.debug("test")
 		i = web.input()
 		n = db.insert('todo', title=i.title)
 		raise web.seeother('/')
 application = web.application(urls, globals()).wsgifunc()
+#if __name__ == "__main__":
+#	app = web.application(urls, globals())
+#	app.run()
