05182e1bd4988cb99dfb89fba277f696068f37b0
shengzhao <zszhaoshengzs@163.com>
2013-01-24 19:54:55 +0800

    add/login.py

--- webpy-app/code.py
+++ webpy-app/code.py
@@ -1,23 +1,89 @@
 import web
+from web.contrib.template import render_mako
 
-render = web.template.render('/home/hadoop/git/webpy-app/templates/')
+web.config.debug = False
 db = web.database(dbn='mysql', user='root', pw='zs', db='recommender')
 urls = (
-	 '/', 'index',
-	 '/add', 'add'
-	 )
-class index:
-	def GET(self):
-		todos = db.select('todo')
-		return render.index(todos)
+		    "/", "login",
+		    "/reset", "reset"
+		)
 
-class add:
+app = web.application(urls, locals())
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'login': 0,'privilege': 0})
+
+# judge the user login or not
+def logged():
+	if session.login==1:
+		return True
+	else:
+		return False
+
+# judge the category of user
+def create_render(privilege):
+	if logged():
+		if privilege==0:
+			render = render_mako(
+		 			directories=['templates/reader'],
+					input_encoding='utf-8',
+					output_encoding='utf-8',
+					)
+		elif privilege==1:
+			render = render_mako(
+					directories=['templates/user'],
+					input_encoding='utf-8',
+					output_encoding='utf-8',
+					)
+		elif privilege==2:
+			render = render_mako(
+					directories=['templates/admin'],
+					input_encoding='utf-8',
+					output_encoding='utf-8',
+					)
+	else:
+		render = render_mako(
+				directories=['templates/commus'],
+				input_encoding='utf-8',
+				output_encoding='utf-8',
+				)
+	return render
+
+
+class login:
+	def GET(self):
+		if logged():
+			render = create_render(session.privilege)
+			return "%s" % (
+					render.login_double()               )
+		else:
+			w
+			render = create_render(session.privilege)
+			return "%s" % (
+					render.login()		                )
 	def POST(self):
-		web.debug("test")
-		i = web.input()
-		n = db.insert('todo', title=i.title)
-		raise web.seeother('/')
-application = web.application(urls, globals()).wsgifunc()
-#if __name__ == "__main__":
-#	app = web.application(urls, globals())
-#	app.run()
+		user, passwd = web.input().user, web.input().passwd
+		ident = db.query("select * from example_user where user = '%s'" %(user)).getresult()
+		try:
+			if passwd==ident[0][2]:
+				session.login=1
+				session.privilege=ident[0][4]
+				render = create_render(session.privilege)
+	 			return "%s" %(render.login_ok())
+			else:
+	 			session.login=0
+				session.privilege=0
+				render = create_render(session.privilege)
+				return "%s" %(render.login_error())
+		except:
+			session.login=0
+			session.privilege=0
+			render = create_render(session.privilege)
+			return "%s" %(render.login_error())
+
+class reset:
+	def GET(self):
+		session.kill()
+		return "%s" %(render.login())
+
+#application = web.application(urls, globals()).wsgifunc()
+if __name__ == "__main__":
+	app.run()
