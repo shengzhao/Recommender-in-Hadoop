0987fe11f455c9362f4a4ef3a1636ec6c6cad0c6
shengzhao <zszhaoshengzs@163.com>
2013-04-04 14:35:00 +0800

    merge

--- Recommender-in-Hadoop/Recommender/src/com/ugc/hadoop/common/PreHandle.java
+++
@@ -1,9 +0,0 @@
-package com.ugc.hadoop.common;
-
-import org.apache.hadoop.fs.Path;
-
-public class PreHandle {
-	public static Path inputPath = null;
-	public static Path outputPath = null;
-	public static Path tempPath = null;
-}
